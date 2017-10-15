c45e38320898f0850f5f633cfbcd9762d26fb056
chukimmuoi <chukimmuoi@gmail.com>
2017-10-15 18:09:31 +0700

    Add UI.

--- app/src/main/java/com/developers/chukimmuoi/indicatorlib/MainActivity.kt
+++ app/src/main/java/com/developers/chukimmuoi/indicatorlib/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.developers.chukimmuoi.indicatorlib
 
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import com.developers.chukimmuoi.indicator.IndicatorView
+import com.developers.chukimmuoi.indicator.PagesLessException
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mViewPager: ViewPager
+    private lateinit var mIndicatorView: IndicatorView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewPager = viewPager
+        mIndicatorView = indicator
+
+        var pagerAdapter = PagerAdapter(applicationContext)
+        mViewPager.adapter = pagerAdapter
+        try {
+            mIndicatorView.setViewpager(mViewPager)
+        } catch (e: PagesLessException) {
+            e.printStackTrace()
+        }
     }
 }
---
+++ app/src/main/java/com/developers/chukimmuoi/indicatorlib/PagerAdapter.kt
@@ -0,0 +1,58 @@
+package com.developers.chukimmuoi.indicatorlib
+
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @author  : Hanet Electronics
+ * @Skype   : chukimmuoi
+ * @Mobile  : +84 167 367 2505
+ * @Email   : muoick@hanet.com
+ * @Website : http://hanet.com/
+ * @Project : IndicatorLib
+ * Created by chukimmuoi on 15/10/2017.
+ */
+class PagerAdapter constructor(private val mContext: Context) : PagerAdapter() {
+
+    override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+        return view == `object`
+    }
+
+    override fun getCount(): Int {
+        return 5
+    }
+
+    override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+        super.destroyItem(container, position, `object`)
+
+        container?.removeView(`object` as View)
+    }
+
+    override fun instantiateItem(container: ViewGroup?, position: Int): Any {
+        var inflater = LayoutInflater.from(mContext)
+        var layout = inflater.inflate(R.layout.page, container, false)
+        when (position) {
+            0 -> {
+                layout.setBackgroundColor(mContext.resources.getColor(R.color.color0))
+            }
+            1 -> {
+                layout.setBackgroundColor(mContext.resources.getColor(R.color.color1))
+            }
+            2 -> {
+                layout.setBackgroundColor(mContext.resources.getColor(R.color.color2))
+            }
+            3 -> {
+                layout.setBackgroundColor(mContext.resources.getColor(R.color.color3))
+            }
+            4 -> {
+                layout.setBackgroundColor(mContext.resources.getColor(R.color.color4))
+            }
+        }
+
+        container?.addView(layout)
+        return layout
+    }
+}
